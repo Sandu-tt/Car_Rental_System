744b7155d2c075a111244f0b8bbf39689ef41797
Sandu-tt <tharakasanu80@gmail.com>
2023-11-02 19:13:56 +0530

    added driverdto

--- Back_End/src/main/java/com/easy/car_rentalsystem/dto/DriverDTO.java
+++ Back_End/src/main/java/com/easy/car_rentalsystem/dto/DriverDTO.java
@@ -1,29 +1,30 @@
 package com.easy.car_rentalsystem.dto;
 
-/**
- * @author : SANDU
- * @project Car_Rental_System
- */
-
+import com.easy.car_rentalsystem.enums.AvailabilityType;
 import embeded.Name;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
 
-
+/**
+ * @author : SANDU
+ * @project Car_Rental_System
+ */
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class AdminDTO {
+public class DriverDTO {
     private String user_Id;
     private Name name;
     private String contact_No;
     private String address;
     private String email;
-    private String nic;
-
-    private UserDTO user;
+    private String nic_No;
+    private String license_No;
+    private MultipartFile license_Img;
+    private AvailabilityType driverAvailability;
 }
