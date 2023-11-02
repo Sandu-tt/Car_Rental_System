1e0034772c6d53c1c582fec5bfef20641018bc19
Sandu-tt <tharakasanu80@gmail.com>
2023-11-02 18:40:46 +0530

    added responseutil

--- Back_End/src/main/java/com/easy/car_rentalsystem/advisor/AppWideExceptionHandler.java
+++ Back_End/src/main/java/com/easy/car_rentalsystem/advisor/AppWideExceptionHandler.java
@@ -1,8 +1,19 @@
 package com.easy.car_rentalsystem.advisor;
 
+import com.easy.car_rentalsystem.util.ResponseUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * @author : SANDU
  * @project Car_Rental_System
  */
 public class AppWideExceptionHandler {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseUtil handleMyExceptions(RuntimeException e) {
+        System.out.println(e);
+        return new ResponseUtil("Error", e.getMessage(), null);
+    }
 }
---
+++ Back_End/src/main/java/com/easy/car_rentalsystem/util/ResponseUtil.java
@@ -0,0 +1,8 @@
+package com.easy.car_rentalsystem.util;
+
+/**
+ * @author : SANDU
+ * @project Car_Rental_System
+ */
+public class ResponseUtil {
+}
